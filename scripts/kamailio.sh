bed46af5c4e135c2539c004bac45f7525ebff652
Loghi Perinpanayagam <loghi@meta.com>
2026-07-19 03:19:25 +0800

    Fix kamailio cfg

--- scripts/kamailio.sh
+++ scripts/kamailio.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 sudo killall kamailio 2>/dev/null; sleep 1
-sudo kamailio -f configs/kamailio/kamailio.cfg -E > /tmp/kamailio.log 2>&1 &
+EXTERNAL_IP=$(curl -s ifconfig.me)
+echo "Kamailio external IP: $EXTERNAL_IP"
+cp configs/kamailio/kamailio.cfg /tmp/kamailio-run.cfg
+sed -i "s/EXTERNAL_IP/$EXTERNAL_IP/g" /tmp/kamailio-run.cfg
+grep "advertise" /tmp/kamailio-run.cfg
+sudo kamailio -f /tmp/kamailio-run.cfg -E > /tmp/kamailio.log 2>&1 &
+sleep 2
 tail -f /tmp/kamailio.log

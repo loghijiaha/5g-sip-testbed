e936cd85fda1a39efa95dd02aa74a789f16d6dd6
Loghi Perinpanayagam <loghi@meta.com>
2026-07-18 19:47:17 +0800

    fixed kamailio

--- scripts/kamailio.sh
+++ scripts/kamailio.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo killall kamailio 2>/dev/null; sleep 1
-sudo kamailio -f /etc/kamailio/kamailio.cfg -E > /tmp/kamailio.log 2>&1 &
+sudo kamailio -f configs/kamailio/kamailio.cfg -E > /tmp/kamailio.log 2>&1 &
 tail -f /tmp/kamailio.log
